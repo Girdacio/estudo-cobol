9e686a870a0bfbdfb57c6df57e0c1dec4907e379
Girdacio <girdacio@gmail.com>
2021-06-17 00:52:38 -0300

    soma atualizado para contemplar numeros negativos

--- spoj-soma.cob
+++ spoj-soma.cob
@@ -4,10 +4,12 @@
         WORKING-STORAGE SECTION.
         01 i        PIC 9(2).
         01 n        PIC 9(2).
-        01 linha    PIC 9(4).
-        01 result   PIC 9(6) VALUE 0.
+        01 linha    PIC S9(5).
+        01 result   PIC S9(7) VALUE 0.
         01 indx     PIC 9(1).
         01 casas    PIC 9(1).
+        01 negativo PIC 9(1) VALUE 0.
+        01 minus-one PIC S9(1) VALUE -1.
         PROCEDURE DIVISION.
         processar.
             ACCEPT n.
@@ -22,7 +24,16 @@
             ACCEPT linha.
             COMPUTE result = result + linha.
         show-result.
+            IF result < 0 THEN
+                SET negativo TO 1
+                MULTIPLY result BY minus-one GIVING result
+            END-IF.
             PERFORM VARYING indx FROM 1 BY 1 
                 UNTIL result(indx:1) <> 0 END-PERFORM.
-            COMPUTE casas = 7 - indx.
-            DISPLAY result(indx:casas).
+            COMPUTE casas = 8 - indx.
+            IF negativo = 1 THEN
+                DISPLAY '-'result(indx:casas)
+            ELSE 
+                DISPLAY result(indx:casas)
+            END-IF.
+    
